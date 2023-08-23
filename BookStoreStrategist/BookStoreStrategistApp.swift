52ea1d4786fa52e20d511a6a38df204014a8ab40
jjkip <jjlangatjose@gmail.com>
2023-08-23 23:34:20 +0300

    Initial Commit

---
+++ BookStoreStrategist/BookStoreStrategistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreStrategistApp.swift
+//  BookStoreStrategist
+//
+//  Created by Joseph Langat on 23/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreStrategistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
