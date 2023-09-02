77779a3f17abfbe48c3717b9a7379e2081dea5a8
jjkip <jjlangatjose@gmail.com>
2023-09-03 00:45:35 +0300

    Displaying Weekly Sales Data

--- BookStoreStrategist/view/sales stats/WeeklySalesChartView.swift
+++ BookStoreStrategist/view/sales stats/WeeklySalesChartView.swift
@@ -6,15 +6,45 @@
 //
 
 import SwiftUI
+import Charts
+
 
 struct WeeklySalesChartView: View {
+    @ObservedObject var salesViewModel: SalesViewModel
+    
+    let salesData: [Sale]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart(salesViewModel.salesByWeek, id: \.day) { saleDate in
+            BarMark(x: .value("Week", saleDate.day, unit: .weekOfYear), y: .value("Sales", saleDate.sales))
+                .foregroundStyle(Color.blue.gradient)
+        }
+    }
+}
+
+struct PlainDataWeeklyChartsView: View {
+    let salesData: [Sale]
+    
+    var body: some View {
+        Chart(salesData) { sale in
+            BarMark(x: .value("Week", sale.saleDate, unit: .weekOfYear), y: .value("Sales", sale.quantity))
+                .foregroundStyle(Color.blue.gradient)
+        }
     }
 }
 
 struct WeeklySalesChartView_Previews: PreviewProvider {
     static var previews: some View {
-        WeeklySalesChartView()
+        WeeklySalesChartView(salesViewModel: SalesViewModel.preview, salesData: Sale.threeMonthsExamples())
+            .aspectRatio(1,contentMode: .fit)
+            .padding()
+    }
+}
+
+struct PlainDataWeeklyChartsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlainDataWeeklyChartsView(salesData: Sale.threeMonthsExamples())
+            .aspectRatio(1,contentMode: .fit)
+            .padding()
     }
 }
