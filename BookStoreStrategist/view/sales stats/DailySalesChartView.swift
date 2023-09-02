e4548f33ea3cfa03d123a6ba8b7aee7c98607c5e
jjkip <jjlangatjose@gmail.com>
2023-09-02 23:59:26 +0300

    Added views for sales data

---
+++ BookStoreStrategist/view/sales stats/DailySalesChartView.swift
@@ -0,0 +1,20 @@
+//
+//  DailySalesChartView.swift
+//  BookStoreStrategist
+//
+//  Created by Joseph Langat on 02/09/2023.
+//
+
+import SwiftUI
+
+struct DailySalesChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DailySalesChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        DailySalesChartView()
+    }
+}
