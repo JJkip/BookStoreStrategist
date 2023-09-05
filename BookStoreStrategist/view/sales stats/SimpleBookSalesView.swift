66f80455edbd0308bc2c66ff44e1f63d2827f749
jjkip <jjlangatjose@gmail.com>
2023-09-05 23:59:48 +0300

    SimpleBookSalesView added

---
+++ BookStoreStrategist/view/sales stats/SimpleBookSalesView.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleBookSalesView.swift
+//  BookStoreStrategist
+//
+//  Created by Joseph Langat on 05/09/2023.
+//
+
+import SwiftUI
+
+struct SimpleBookSalesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SimpleBookSalesView_Previews: PreviewProvider {
+    static var previews: some View {
+        SimpleBookSalesView()
+    }
+}
